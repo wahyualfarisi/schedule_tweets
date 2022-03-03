5036fc5754976e3d4e3e48325b0dc5c12879fa3f
wahyu alfarisi <wahyualfarisi@gmail.com>
2022-03-03 21:07:53 +0700

    add changes test

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   root to: "main#index"
 
+  get "sign_up", to: "registration#new"
+  post "sign_up", to: "registration#create"
+
   get "/about", to: "about#index"
 
 end
